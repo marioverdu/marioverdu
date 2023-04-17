af1a0c032ca9d6244dd8d47e1b069c66452e629b
marioverdu <marioverdugambin@gmail.com>
2023-04-17 12:10:09 +0200

    Delete ga.jspm.io directory

--- ga.jspm.io/npm@motionone/generators@10.12.0/dist/utils/has-reached-target.es.js
+++
@@ -1,8 +0,0 @@
-function hasReachedTarget(origin, target, current) {
-  return (
-    (origin < target && current >= target) ||
-    (origin > target && current <= target)
-  );
-}
-
-export { hasReachedTarget };
--- ga.jspm.io/npmreact@18.1.0/jsx-runtime.js
+++
@@ -1,5 +0,0 @@
-import _reactJsxRuntimeProductionMin from "./cjs/react-jsx-runtime.production.min.js";
-export { _reactJsxRuntimeProductionMin as default };
-export const Fragment = _reactJsxRuntimeProductionMin.Fragment,
-  jsx = _reactJsxRuntimeProductionMin.jsx,
-  jsxs = _reactJsxRuntimeProductionMin.jsxs;
