49e6b3f035898067d4c72a81b75c1fe2a18472d8
marioverdu <marioverdugambin@gmail.com>
2023-09-26 22:01:52 +0200

    v39.1

---
+++ framerusercontent.com/sites/1LvMe8AUirzwLDfGyIzIfr/chunk-FPVHB5BZ.mjs
@@ -0,0 +1,2 @@
+var a=e=>({description:"Dise\xF1o de productos digitales enfocado en el usuario",favicon:new URL("https://framerusercontent.com/images/JcHxk4ch2Q3cwPgWpUGQLKmuLc.png").href,title:"Mario Verd\xFA | Dise\xF1ador UX/UI"}),n=a;export{n as a};
+//# sourceMappingURL=chunk-FPVHB5BZ.mjs.map
