1a1628c7269ae8ba8ff86cfdfec3d1e5e890a273
marioverdu <marioverdugambin@gmail.com>
2023-10-06 14:51:24 +0200

    v39.41

---
+++ framerusercontent.com/sites/3CxmDB4afL7lvyfhzGAu0Q/chunk-DZBRTYPE.mjs
@@ -0,0 +1,2 @@
+function a(t,e){return{description:"Dise\xF1o de productos digitales enfocado en el usuario",favicon:"https://framerusercontent.com/images/JcHxk4ch2Q3cwPgWpUGQLKmuLc.png",title:"Mario Verd\xFA | Dise\xF1o de Productos Digitales"}}export{a};
+//# sourceMappingURL=chunk-DZBRTYPE.mjs.map
