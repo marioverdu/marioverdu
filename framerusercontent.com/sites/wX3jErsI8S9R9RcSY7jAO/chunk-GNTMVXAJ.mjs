27c9e4bf18ab9ad8a8d05b62c0ba100ab2876f2c
marioverdu <marioverdugambin@gmail.com>
2023-11-13 12:43:41 +0100

    Empty State - Not available. WIP

---
+++ framerusercontent.com/sites/wX3jErsI8S9R9RcSY7jAO/chunk-GNTMVXAJ.mjs
@@ -0,0 +1,2 @@
+function t(e,a){return{description:"User-focused digital product design",title:"Mario Verd\xFA | Dise\xF1o de Productos Digitales"}}export{t as a};
+//# sourceMappingURL=chunk-GNTMVXAJ.mjs.map
